1354653faac98be444a2f8443fb08d17f8f145f3
root <root@insight-journal.org>
2011-08-22 09:47:18 -0400

    revision 1

---
+++ Source/CMakeFiles/shapeAnalysisMANCOVA.dir/DependInfo.cmake
@@ -0,0 +1,17 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/MANCOVA.cxx" "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/CMakeFiles/shapeAnalysisMANCOVA.dir/MANCOVA.o"
+  "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/itkMeshTovtkPolyData.cxx" "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/CMakeFiles/shapeAnalysisMANCOVA.dir/itkMeshTovtkPolyData.o"
+  "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/miscMANCOVA.cxx" "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/CMakeFiles/shapeAnalysisMANCOVA.dir/miscMANCOVA.o"
+  "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/nonMANCOVA.cxx" "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/CMakeFiles/shapeAnalysisMANCOVA.dir/nonMANCOVA.o"
+  "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/shapeAnalysisMANCOVA.cxx" "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/Source/CMakeFiles/shapeAnalysisMANCOVA.dir/shapeAnalysisMANCOVA.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ Source/CMakeFiles/shapeAnalysisMANCOVA.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/shapeAnalysisMANCOVA.dir/shapeAnalysisMANCOVA.o"
+  "CMakeFiles/shapeAnalysisMANCOVA.dir/MANCOVA.o"
+  "CMakeFiles/shapeAnalysisMANCOVA.dir/itkMeshTovtkPolyData.o"
+  "CMakeFiles/shapeAnalysisMANCOVA.dir/miscMANCOVA.o"
+  "CMakeFiles/shapeAnalysisMANCOVA.dir/nonMANCOVA.o"
+  "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/bin/shapeAnalysisMANCOVA.pdb"
+  "/home/paniagua/Research/MANCOVA/InsightJournal/Submission09042009/bin/shapeAnalysisMANCOVA"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/shapeAnalysisMANCOVA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
